673409c4ca6ed6b5c26adb07df767c04fb4a8a74
datsun80zx <m.richardp93@gmail.com>
2025-10-05 08:46:49 -0400

    added the first simple handlerfunc

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	testServ := &http.Server{
 		Addr:    ":" + port,
